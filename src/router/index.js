6aa3c1333a9ae8a40d8dc4951ebfc8760c35169a
Brian Kariuki <bkariuki@CT-BKARIUKI.local>
2017-02-23 17:57:17 +0300

    advanced concepts

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import product from 'components/product'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'product',
+      component: product
+    }
+  ]
+})
